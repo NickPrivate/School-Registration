d08ef49fce9906082c20d76916a73b949859429a
NickPrivate <dragonwolf478@gmail.com>
2023-11-12 23:37:49 -0800

    testing file upload bug

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include <utility>
+#include <unordered_map>
+
+struct Class
+{
+
+    std::string className;
+    std::string classTime;
+    int classNum;
+
+    Class(std::string& newClasssName, std::string& newClassTime, int & newClassNum){
+
+        className = newClasssName;
+        classTime = newClassTime;
+        classNum  = newClassNum;
+
+    }
+
+     std::pair<int,Class> addClass (std::pair<int,Class> myClass){
+        
+     }
+    
+    
+};
+
+
+
+int main(){
+
+    std::string className;
+    std::string classTime;
+    int classNum;
+
+    Class classes( className, classTime, classNum);
+
+    std::unique_ptr<std::unordered_map<int,Class>> classMap;
+
+    for (const auto& i : *classMap){
+
+        std::cout << "Type the class name, class time, and class num\n";
+        std::cin >> className;
+        std::cin >> classTime;
+        std::cin >> classNum;
+
+    Class classes( className, classTime, classNum);
+
+        if (classMap->find(i.first) != classMap->end()) break;
+
+        classMap->insert(i);
+
+
+    }
+
+
+
+    return 0;
+}
